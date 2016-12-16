b138272b6bad4c0748a1cd5a68291e10b92c9a3b
Cole Allan <cacoleallan@gmail.com>
2016-12-15 17:36:41 -0700

    Completed the intermediate assessment practice

--- intermediate-js-review/review.js
+++ intermediate-js-review/review.js
@@ -21,30 +21,38 @@ function theOverlord() {
 
 //Assign your answers to the variables below.
 //1. The value of rules if accessed from thePowerlessFigurehead
-var ans1 = '';
+var ans1 = 'love me plz';
 //2. The value of rules if accessed from theJudge
-var ans2 = '';
+var ans2 = 'cry all day';
 //3. The value of job if accessed from theOverlord
-var ans3 = '';
+var ans3 = undefined;
 //4. The value of job if accessed from thePowerlessFigurehead
-var ans4 = '';
+var ans4 = undefined;
 //5. The value of money if accessed from theTragicHumanitarian
-var ans5 = '';
+var ans5 = 'belongs to the overlord';
 
 ///Promises///
+
+
+this.iPromise = function(fib){
+  var defer = $q.defer();
+  return defer.promise;
+  defer.resolve(fib);
+}
+
 
 //6. Write a function called iPromise. Use $q to create
 /////a promise and return the deferred promise. Find the 38th
 /////number in the Fibonacci sequence and resolve the promise
 /////with the answer when it completes.
 /////Fibonacci sequence:
-/////var fibonacci = function(n) {
-//     if(n <= 2) {
-//         return 1;
-//     } else {
-//         return this.recursive(n - 1) + this.recursive(n - 2);
-//     }
-// };
+  var fibonacci = function(n) {
+    if(n <= 2) {
+        return 1;
+    } else {
+        return this.recursive(n - 1) + this.recursive(n - 2);
+    }
+};
 
 
 
@@ -54,19 +62,22 @@ var ans5 = '';
 /////to the contextObj, and with the two parameters passed in - return
 /////the result.
 function contextIt(myFn, contextObj, param1, param2) {
-
+  return myFn.call(contextObj, param1, param2);
 }
 
 //8. Do the same as you did in question 6, except with the
 /////paramArray passed in instead
 function contextIt2(myFn, contextObj, paramArray) {
+return myFn.apply(contextObj, paramArray);
 
 }
+
 
 //9. Inside contextIt3, return a new function that permanently
 /////links myFn to the contextObj
 function contextIt3(myFn, contextObj) {
-
+  var func = myFn.bind(contextObj);
+  return func;
 }
 
 
@@ -77,11 +88,30 @@ var bubbles = 0; //Ignore this line except to mourn our lack of bubbles.
 /////parameters called hornColor, magicType, mana, and
 /////canFly; inside the function, assign each to an identically
 /////named property
+function Unicorn(hornColor, magicType, mana, canFly){
+  this.hornColor = hornColor;
+  this.magicType = magicType;
+  this.mana = mana;
+  this.canFly = canFly;
+  this.castBubbleWrapSpell = function(){
+  if(this.mana > 0){
+    bubbles += 100;
+    this.mana -= 15;
+  }
+
+  }
+}
 
 //11. Add a prototype method to Unicorn called castBubbleWrapSpell
 /////which adds 100 to the global variable bubbles and takes away
 /////15 from mana on the unicorn until it hits zero.
 
+Unicorn.prototype.castBubbleWrapSpell = function(){
+  bubbles += 100;
+  while(this.mana > 0){
+    mana -= 15;
+  }
+}
 
 
 /////Closures/////
@@ -89,6 +119,13 @@ var bubbles = 0; //Ignore this line except to mourn our lack of bubbles.
 //12. Write a function called greetingMaker which takes in a name
 /////and returns a function that takes in a greeting and returns
 /////the name and the greeting concatenated together (in that order)
+
+function greetingMaker(name){
+  return function(greeting){
+    return name + greeting;
+  }
+}
+
 
 
 //13. Write a function called countdownMaker which takes in an
@@ -98,6 +135,16 @@ var bubbles = 0; //Ignore this line except to mourn our lack of bubbles.
 /////object that looks like this:
 /////{event: <eventname>, daysLeft: <days left>}
 
+function countdownMaker(str, days){
+  return function(){
+    days--;
+    var obj = {
+      event: str,
+      daysLeft: days
+    }
+    return obj;
+  }
+}
 
 
 /////Type Checking/////
@@ -105,3 +152,7 @@ var bubbles = 0; //Ignore this line except to mourn our lack of bubbles.
 //14. Write a function called notMyType which takes in a parameter
 /////and returns the type of data of the parameter (number, string,
 /////boolean, etc)
+
+function notMyType(param){
+  return typeof(param);
+}
